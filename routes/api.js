5a943606a181616dfb281598aeabb91e87e932ca
tdm <55875354+taraDM23@users.noreply.github.com>
2020-02-05 14:15:30 +1100

    renamed const connection to router

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,29 @@
-const app = require("express").Router();
+const router = require("express").Router();
+const appClass = require("../db/appClass");
 
-// GET a note
-app.get("/notes", function (req, res) {
-  appClass
-    .getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err));
-});
 
 //POST a note
-app.post("/notes", (req, res) => {
+router.post("/notes", (req, res) => {
   appClass
     .postNote(req.body)
     .then((note) => res.json(note))
     .catch(err => res.status(500).json(err));
 });
 
+// GET a note
+router.get("/notes", function (req, res) {
+  appClass
+    .getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
 
 // DELETE a note
-app.delete("/notes/:id", function (req, res) {
+router.delete("/notes/:id", function (req, res) {
   appClass
     .deleteNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch(err => res.status(500).json(err));
 });
 
-module.exports = app;
+module.exports = router;
--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,13 @@
-var path = require("path");
-var app = require("express").Router();
+const path = require("path");
+const router = require("express").Router();
 
 // link to html files
-app.get("/notes", function(req, res) {
+router.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-app.get("", function(req, res) {
+router.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-module.exports = app;
+module.exports = router;
